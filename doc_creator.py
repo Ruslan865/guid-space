eda664c03517351604085cf6a6e4d7e0562ae6c0
Ruslan865 <babashov666@gmail.com>
2025-07-05 09:12:27 -0700

    Update doc_creator.py

--- doc_creator.py
+++ doc_creator.py
@@ -1,12 +1,17 @@
 from fpdf import FPDF
 
-def save_to_pdf(title, contents):
+def save_to_pdf(title, lines, filename="output.pdf"):
     pdf = FPDF()
     pdf.add_page()
-    pdf.set_font("Arial", size=12)
-    pdf.cell(200, 10, txt=title, ln=1, align='C')
 
-    for line in contents:
+    # Unicode font əlavə et (məsələn, DejaVu)
+    pdf.add_font('DejaVu', '', 'DejaVuSans.ttf', uni=True)
+    pdf.set_font("DejaVu", size=12)
+
+    pdf.cell(200, 10, txt=title, ln=True, align='C')
+    pdf.ln(10)
+
+    for line in lines:
         pdf.multi_cell(0, 10, txt=line)
 
-    pdf.output("output.pdf")
+    pdf.output(filename)
